098e55f19068cfeb9ffe96d3b3a21183fe852259
BlackRockCoder <447288941@qq.com>
2020-03-26 17:58:09 +0800

    Push for boat

--- nachos/threads/Boat.java
+++ nachos/threads/Boat.java
@@ -4,36 +4,82 @@ import nachos.ag.BoatGrader;
 public class Boat {
   static BoatGrader bg;
 
+  static KThread parent_thread;
+  static int child_O_num;
+  static int child_M_num;
+  static int adult_O_num;
+  static int adult_M_num;
+  static boolean has_pilot;
+  static int boat;
+  static Lock lock;
+  static Condition con_child_O;
+  static Condition con_child_M;
+  static Condition con_adult;
+  static Condition finish;
+
   public static void selfTest() {
     BoatGrader b = new BoatGrader();
+	
+    begin(3, 3, b);
 
-    System.out.println("\n ***Testing Boats with only 2 children***");
+    begin(10, 9, b);
+
+    begin(17, 5, b);
+
+    begin(5, 20, b);
+
     begin(0, 2, b);
 
-    //	System.out.println("\n ***Testing Boats with 2 children, 1 adult***");
-    //  	begin(1, 2, b);
-
-    //  	System.out.println("\n ***Testing Boats with 3 children, 3
-    //  adults***");
-    //  	begin(3, 3, b);
+    begin(20, 2, b);
   }
 
   public static void begin(int adults, int children, BoatGrader b) {
     // Store the externally generated autograder in a class
     // variable to be accessible by children.
     bg = b;
+	
+	parent_thread = KThread.currentThread();
+	child_O_num = children;
+	adult_O_num = adults;
+	child_M_num = 0;
+	adult_M_num = 0;
+	has_pilot = false;
+	boat = 0; 
+	//boat 0 in O, 1 in M
+	lock = new Lock();
+	con_adult = new Condition(lock); //Condition 2
+	con_child_O = new Condition(lock); //Condition 1
+	con_child_M = new Condition(lock); //Condition 3
+	finish = new Condition(lock);
 
     // Instantiate global variables here
+	
+	lock.acquire();
 
-    // Create threads here. See section 3.4 of the Nachos for Java
+	for (int i = 1; i <= adults; i++){
+		Runnable r = new Runnable() { 
+			public void run() { AdultItinerary(); }
+  		};
+		KThread t = new KThread(r);
+		t.setName("Adult Thread " + i);
+		t.fork();
+	}
+
+ 	for (int i = 1; i <= children; i++){
+		Runnable r = new Runnable() { 
+			public void run() { ChildItinerary(); }
+  		};
+		KThread t = new KThread(r);
+		t.setName("Child Thread " + i);
+		t.fork();
+	}			
+	
+	finish.sleep();
+
+	lock.release();
+	 // Create threads here. See section 3.4 of the Nachos for Java
     // Walkthrough linked from the projects page.
 
-    Runnable r = new Runnable() {
-      public void run() { SampleItinerary(); }
-    };
-    KThread t = new KThread(r);
-    t.setName("Sample Boat Thread");
-    t.fork();
   }
 
   static void AdultItinerary() {
@@ -47,12 +93,69 @@ public class Boat {
            bg.AdultRowToMolokai();
        indicates that an adult has rowed the boat across to Molokai
     */
+	
+	lock.acquire();
+
+	while (boat == 1 || child_O_num >= 2)
+		con_adult.sleep();
+	bg.AdultRowToMolokai();
+	adult_O_num--;
+	adult_M_num++;
+	boat ^= 1;
+	con_child_M.wake();
+
+	lock.release();
   }
 
   static void ChildItinerary() {
+
     bg.initializeChild(); // Required for autograder interface. Must be the
                           // first thing called.
     // DO NOT PUT ANYTHING ABOVE THIS LINE.
+	lock.acquire();
+
+	int place = 0;
+	while (place != -1){ // Same thing as while(true), but while(true) will make compile error
+		
+		if (place == 0){
+			while ((boat == 1 || child_O_num < 2) && !has_pilot)
+				con_child_O.sleep();
+			child_O_num--;
+			child_M_num++;
+			if (!has_pilot){
+				bg.ChildRowToMolokai();
+				has_pilot = true;
+				con_child_O.wake();
+			} else {
+				bg.ChildRideToMolokai();
+				has_pilot = false;
+				boat ^= 1;
+				if (child_O_num == 0 && adult_O_num == 0)
+					finish.wake();
+				else 
+					con_child_M.wake();
+			}
+
+			con_child_M.sleep();
+		} else {
+			while (boat == 0) con_child_M.sleep();
+		
+			child_O_num++;
+			child_M_num--;
+			bg.ChildRowToOahu();
+			boat ^= 1;
+			
+			if (child_O_num < 2)
+				con_adult.wake();
+			else	
+				con_child_O.wake();
+
+			con_child_O.sleep();
+		}
+		place ^= 1;
+	}	
+
+	lock.release();
   }
 
   static void SampleItinerary() {
